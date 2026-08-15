2cc6e347b8f8474bc97e2484e326a6b82c67542e
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-15 21:51:31 +0300

    Add Movie model

---
+++ app/models/movie.py
@@ -0,0 +1,36 @@
+import uuid
+from decimal import Decimal
+
+from sqlalchemy import DECIMAL, Float, ForeignKey, Integer, String, Text, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    __table_args__ = (
+        UniqueConstraint("name", "year", "time"),
+    )
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    uuid: Mapped[uuid.UUID] = mapped_column(
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    time: Mapped[int] = mapped_column(Integer, nullable=False)
+    imdb: Mapped[float] = mapped_column(Float, nullable=False)
+    votes: Mapped[int] = mapped_column(Integer, nullable=False)
+    meta_score: Mapped[float | None] = mapped_column(Float, nullable=True)
+    gross: Mapped[float | None] = mapped_column(Float, nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    price: Mapped[Decimal] = mapped_column(DECIMAL(10, 2), nullable=False)
+    certification_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("certifications.id"),
+        nullable=False,
+    )
