7d049d0588057adf4c41a9b98dea8ca8738db1bf
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-15 21:44:43 +0300

    Add Star model

---
+++ app/models/star.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Star(Base):
+    __tablename__ = "stars"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False,
+    )
