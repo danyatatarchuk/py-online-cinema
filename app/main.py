bf4072e1235d6c61d3310942dbde8b9196aba839
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2026-08-16 01:31:25 +0300

    Register movies router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.routers.auth import router as auth_router
+from app.routers.movies import router as movies_router
 
 app = FastAPI(
     title="Online Cinema API",
@@ -9,6 +10,7 @@ app = FastAPI(
 )
 
 app.include_router(auth_router)
+app.include_router(movies_router)
 
 
 @app.get("/")
